15f73c47b1c8ea1ac39a00752ddfb18f1c29108a
name_fengzi@sina.com <name_fengzi@sina.com>
2022-02-19 23:21:12 +0800

    消息通知

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 #### 可以发送微信通知、钉钉通知、邮箱通知、短信通知等...
+
+````php
+<?php
+
+include "../vendor/autoload.php";
+use Notice\Notice\NoticeFactory;
+
+//发送钉钉消息示例
+NoticeFactory::dinDin([
+  'webHookUrl' => ''
+])->text('报警：一个测试的消息');
+
+//NoticeFactory::wechat(); 微信通知
+//NoticeFactory::sms();    短信通知
+//NoticeFactory::email();  邮件通知
+````
+- 目前暂时接入了钉钉通知，后续会逐步完善其他几个
--- src/test.php
+++ src/test.php
@@ -4,11 +4,11 @@ include "../vendor/autoload.php";
 use Notice\Notice\NoticeFactory;
 
 NoticeFactory::dinDin([
-  'webHookUrl' => 'https://oapi.dingtalk.com/robot/send?access_token=3d141b998d0439b72bb3b4fa8dbf3892b0e0ba374e612637de78ee3b1a7695ef'
-])->text('报警：一个测试的消息', [15869020161]);
+  'webHookUrl' => ''
+])->text('报警：一个测试的消息');
 
-NoticeFactory::wechat();
-NoticeFactory::sms();
-NoticeFactory::email();
+//NoticeFactory::wechat();
+//NoticeFactory::sms();
+//NoticeFactory::email();
 
 
