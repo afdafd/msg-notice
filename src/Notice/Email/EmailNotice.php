51adca2e5c924e0c8d84067da98cd0778ad48c9f
name_fengzi@sina.com <name_fengzi@sina.com>
2022-08-19 11:39:08 +0800

    新增邮件发送功能（支持附件、文本）

--- src/Notice/Email/EmailNotice.php
+++ src/Notice/Email/EmailNotice.php
@@ -12,8 +12,110 @@ use Notice\Notice\Notice;
  */
 class EmailNotice extends Notice
 {
-  public function sendMsgNotice(array $message)
-  {
+    protected $baseConfig = [];
 
-  }
+    public function __construct(array $config = [])
+    {
+        if (empty($config)) {
+            throw new \Exception('配置信息不能为空');
+        }
+
+       $this->baseConfig = $config;
+    }
+
+    /**
+     * 发送附件邮箱
+     *
+     * @param array $receive 接收者
+     * @param string $resourcePath 资源路径
+     * @param string $subject  描述
+     * @param array $send 发送者
+     * @param string $bodyContent 主体内容
+     * @param bool $isCc 是否回传
+     * @param array $ccPath 回传地址
+     * @return int
+     */
+    public function sendAttach(
+        array $receive,
+        string $resourcePath,
+        string $subject,
+        array $send = [],
+        string $bodyContent = '',
+        bool $isCc = false,
+        array $ccPath = []
+    ): int
+    {
+        $fromAddress = $send['from_address'] ?? $this->baseConfig['from_address'];
+        $fromName = $send['from_name'] ?? $this->baseConfig['from_name'];
+
+        $message = (new \Swift_Message($subject, $bodyContent, null, 'UTF-8'))
+            ->setFrom([$fromAddress => $fromName])
+            ->setTo($receive)
+            ->attach(\Swift_Attachment::fromPath($resourcePath));
+
+        if ($isCc) {
+            if (!empty($ccPath)) {
+                $path = $ccPath['path'] ?? '';
+                $name = $ccPath['name'] ?? null;
+                $message->setCc($path, $name);
+            }
+        }
+
+        return $this->getMailer()->send($message);
+    }
+
+    /**
+     * 发送文本
+     * @param array $receive
+     * @param string $subject
+     * @param string $bodyContent
+     * @param array $send
+     * @param bool $isCc
+     * @param array $ccPath
+     * @return int
+     */
+    public function sendText(
+        array $receive,
+        string $subject,
+        string $bodyContent,
+        array $send = [],
+        bool $isCc = false,
+        array $ccPath = []
+    ): int
+    {
+        $fromAddress = $send['from_address'] ?? $this->baseConfig['from_address'];
+        $fromName = $send['from_name'] ?? $this->baseConfig['from_name'];
+
+        $message = (new \Swift_Message($subject))
+            ->setFrom([$fromAddress => $fromName])
+            ->setTo($receive)
+            ->setBody($bodyContent)
+            ->setCharset('UTF-8');
+
+        if ($isCc) {
+            if (!empty($ccPath)) {
+                $path = $ccPath['path'] ?? '';
+                $name = $ccPath['name'] ?? null;
+                $message->setCc($path, $name);
+            }
+        }
+
+        return $this->getMailer()->send($message);
+    }
+
+    /**
+     * @return \Swift_Mailer
+     */
+    private function getMailer(): \Swift_Mailer
+    {
+        $transport = (new \Swift_SmtpTransport(
+            $this->baseConfig['host'],
+            $this->baseConfig['port'],
+            $this->baseConfig['encryption']
+        ))
+        ->setUsername($this->baseConfig['username'])
+        ->setPassword($this->baseConfig['password']);
+
+       return new \Swift_Mailer($transport);
+    }
 }
--- src/test.php
+++ src/test.php
@@ -3,14 +3,16 @@
 include "../vendor/autoload.php";
 use Notice\Notice\NoticeFactory;
 
-$dindin = NoticeFactory::dinDin([
-  'webHookUrl' => ''
-]);
-
-//文本类型
-$dindin->text('报警：一个测试的消息');
-//link 类型
-$dindin->link('报警：我是link', '我是一个测试的link', 'http://www.baidu.com');
+//
+//$dindin = NoticeFactory::dinDin([
+//  'webHookUrl' => 'https://oapi.dingtalk.com/robot/send?access_token=3d141b998d0439b72bb3b4fa8dbf3892b0e0ba374e612637de78ee3b1a7695ef'
+//]);
+//
+//
+////文本类型
+//$dindin->text('报警：一个测试的消息');
+////link 类型
+//$dindin->link('报警：我是link', '我是一个测试的link', 'http://www.baidu.com');
 //$dindin->markdown();
 //$dindin->actionCard();
 //$dindin->feedCard();
@@ -21,3 +23,17 @@ $dindin->link('报警：我是link', '我是一个测试的link', 'http://www.ba
 //NoticeFactory::email();
 
 
+$a = [
+    "host" => "xxx",
+    "port" => 666,
+    "encryption" => "ssl",
+    "from_address" => "xxx",
+    "username" => "xxx",
+    "password" => "xxx",
+    "from_name" => "xxx"
+];
+NoticeFactory::email($a)->sendAttach(
+    ['test@163.com'],
+    '/test.docx',
+    '哈哈哈哈_test'
+);
